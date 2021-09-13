9a567320a1e6675582ccd40cafedc49c807a8942
Ricardo Flores <rik.fm1809@gmail.com>
2021-09-13 14:23:05 -0500

    Install and config Eslint up2

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
     ],
     "rules": {
         "react/prop-types": "off",
-        "react/react-in-jsx-scope": "off"
+        "react/react-in-jsx-scope": "off",
+        // "@typescript-eslint/explicit-module-boundary-types": "off"
     }
 };
